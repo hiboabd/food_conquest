95cd25ff0f60902430f9d4982821076308bbedca
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-09 10:05:47 +0100

    Feat: Add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Home />
       </header>
     </div>
   );
