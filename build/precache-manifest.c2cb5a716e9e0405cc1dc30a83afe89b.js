9f35c7cabe792e26b2a563687cb3be2440dc8737
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-09 14:24:14 +0100

    Create production build of app

---
+++ build/precache-manifest.c2cb5a716e9e0405cc1dc30a83afe89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb0285bebec60c938d0242c55eb3373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b041bb347ac53cca1b",
+    "url": "/static/css/main.af606211.chunk.css"
+  },
+  {
+    "revision": "5853eb5a96d62e98ab2f",
+    "url": "/static/js/2.d4ab4fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4ab4fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b041bb347ac53cca1b",
+    "url": "/static/js/main.9a23d924.chunk.js"
+  },
+  {
+    "revision": "3e22591f3a32ca4bee8a",
+    "url": "/static/js/runtime-main.fa558dd7.js"
+  }
+]);
